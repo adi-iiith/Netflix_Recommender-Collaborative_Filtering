11151c60cead62e23321f1836b66a0543d8ddf60
Aditya Tewari <aditya.tewari@students.iiit.ac.in>
2019-04-20 12:51:38 +0530

    data read implemented, csr format

--- svdpp.py
+++ svdpp.py
@@ -8,6 +8,37 @@ import math
 
 file_name = "../input/ml-100k.data"
 
+def mapping(train) :
+    uid_dict = {}
+    iid_dict = {}
+    current_u_index = 0
+    current_i_index = 0
+
+    row = []
+    col = []
+    data = []
+    for urid,irid,r,timestamp in train :
+
+    	try:
+    		uid = uid_dict[urid]
+    	except KeyError:
+    		uid = current_u_index
+    		uid_dict[urid] = current_u_index
+    		current_u_index += 1
+    	try:
+    		iid = iid_dict[irid]
+    	except KeyError:
+    		iid = current_i_index
+    		iid_dict[irid] = current_i_index
+    		current_i_index += 1
+
+    	row.append(uid)
+    	col.append(iid)
+    	data.append(r)
+
+    train_sparse = csr_matrix((data, (row, col)))
+    return train_sparse, uid_dict, iid_dict
+    
 def parse_line(line):
     line = line.split("\t")
     uid, iid, r, timestamp = (line[i].strip() for i in range(4))
@@ -26,5 +57,10 @@ def Read_Data(file_name,shuffle=True) :
 		np.random.shuffle(raw_ratings)
 
 	raw_len = len(raw_ratings)
+
+	train_sparse,uid,iid = mapping(raw_ratings[:math.ceil(raw_len*0.8)])
+	test = raw_ratings[math.ceil(raw_len*0.8):]
+
+	return train_sparse,uid,iid,test
 
 	
