f392dfa3bee760e61654f7dc474bc1a1860189a4
Aditya Tewari <aditya.tewari@students.iiit.ac.in>
2019-04-21 01:13:59 +0530

    data read from file, pandas time expensive

--- integrated_model.py
+++ integrated_model.py
@@ -8,10 +8,25 @@ import math
 
 file_name = "../input/ml-100k.data"
 
+def parse_line(line):
+    line = line.split("\t")
+    uid, iid, r, timestamp = (line[i].strip() for i in range(4))
+    return uid, iid, float(r), timestamp
 
 def Read_Data(file_name,shuffle=True) :
 
-	data = pd.read_csv(file_name,sep = "\t", names = ["uid","iid","r","timst"])
-	data.sort_values(by = "timst", inplace = True)
+	# data = pd.read_csv(file_name,sep = "\t", names = ["uid","iid","r","timst"])
+	# data.sort_values(by = "timst", inplace = True)
 
-	train = data.head(n = int(0.8 * da
+	# train = data.head(n = int(0.8 * data.shape[0]))
+	# test = data.drop(train.index)
+	with open(os.path.expanduser(file_name)) as f:
+		raw_ratings = [parse_line(line) for line in itertools.islice(f, 0, None)]
+	if shuffle:
+		np.random.shuffle(raw_ratings)
+
+	raw_len = len(raw_ratings)
+
+	train_sparse,uid,iid = mapping(raw_ratings[:math.ceil(raw_len*0.8)])
+	test = raw_ratings[math.ceil(raw_len*0.8):]
+	return train_sparse,uid,iid,test
