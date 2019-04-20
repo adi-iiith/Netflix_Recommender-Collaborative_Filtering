b0d51db8c41b6e6a94f30327e2cfa8fd832b438e
Aditya Tewari <aditya.tewari@students.iiit.ac.in>
2019-04-21 01:17:52 +0530

    retrieve from sparse matrix item,rating,users

--- integrated_model.py
+++ integrated_model.py
@@ -30,3 +30,25 @@ def Read_Data(file_name,shuffle=True) :
 	train_sparse,uid,iid = mapping(raw_ratings[:math.ceil(raw_len*0.8)])
 	test = raw_ratings[math.ceil(raw_len*0.8):]
 	return train_sparse,uid,iid,test
+
+
+def all_ratings(matrix,axis=1):
+
+    coo_matrix = matrix.tocoo()
+
+    if axis == 1:
+        return zip(coo_matrix.row, coo_matrix.col, coo_matrix.data)
+    else:
+        return coo_matrix.row, coo_matrix.col, coo_matrix.data
+
+def get_user(matrix, u):
+
+    ratings = matrix.getrow(u).tocoo()
+    return ratings.col, ratings.data
+
+def get_item(matrix, i):
+
+
+    ratings = matrix.getcol(i).tocoo()
+    return ratings.row, ratings.data
+
