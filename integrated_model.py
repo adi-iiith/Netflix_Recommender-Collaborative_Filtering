4f01a1adfd22a798c2060672cfe3712fbe58cffe
Aditya Tewari <aditya.tewari@students.iiit.ac.in>
2019-04-21 16:24:27 +0530

    mapping function included

--- integrated_model.py
+++ integrated_model.py
@@ -8,6 +8,38 @@ import math
 
 file_name = "../input/ml-100k.data"
 
+def mapping(train) :
+    uid_dict = {}
+    iid_dict = {}
+    current_u_index = 0
+    current_i_index = 0
+
+    row = []
+    col = []
+    data = []
+
+    for urid,irid,r,timestamp in train :
+
+        try:
+            uid = uid_dict[urid]
+        except KeyError:
+            uid = current_u_index
+            uid_dict[urid] = current_u_index
+            current_u_index += 1
+        try:
+            iid = iid_dict[irid]
+        except KeyError:
+            iid = current_i_index
+            iid_dict[irid] = current_i_index
+            current_i_index += 1
+
+        row.append(uid)
+        col.append(iid)
+        data.append(r)
+
+    train_sparse = csr_matrix((data, (row, col)))
+    return train_sparse, uid_dict, iid_dict
+
 def parse_line(line):
     line = line.split("\t")
     uid, iid, r, timestamp = (line[i].strip() for i in range(4))
